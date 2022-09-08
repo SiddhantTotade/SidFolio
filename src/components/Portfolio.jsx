060eb63126c6cf33e237411c19270b97e1520339
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-09-09 01:34:31 +0530

    portfolio pagedone

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,19 +11,28 @@ export const Portfolio = () => {
     const portfolios = [
         {
             id: 1,
-            src: amazonClone
+            src: amazonClone,
+            code: `https://github.com/SiddhantTotade/amazon_clone`,
+            demo: `https://amazon-clone-1994.herokuapp.com/`
         },
         {
             id: 2,
-            src: periodicTableGUI
+            src: periodicTableGUI,
+            code: `https://github.com/SiddhantTotade/Periodic-Table-Java-GUI`,
+            demo: `https://github.com/SiddhantTotade/Periodic-Table-Java-GUI`
+
         },
         {
             id: 3,
-            src: videoChatting
+            src: videoChatting,
+            code: `https://github.com/SiddhantTotade/video_chat_app_deploy`,
+            demo: `https://pyvid.herokuapp.com/`
         },
         {
             id: 4,
-            src: todoApplication
+            src: todoApplication,
+            code: `https://github.com/SiddhantTotade/todo_list`,
+            demo: `https://my-work-list.herokuapp.com/`
         },
         // {
         //     id: 5,
@@ -36,19 +45,18 @@ export const Portfolio = () => {
     ];
 
     return (
-        <div name="portfolio" className='bg-white w-full text-stone-600 md:h-screen'>
+        <div name="portfolio" className='bg-white w-full text-gray-600 md:h-screen'>
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
-                    <p className='py-6'>Check out some of my work right here.</p>
                 </div>
-                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-                    {portfolios.map(({ id, src }) => (
+                <div className='py-6 grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+                    {portfolios.map(({ id, src, demo, code }) => (
                         <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
                             <img src={src} alt="" className='rounded-md hover:scale-150 duration-200' />
                             <div className='flex items-center justify-center'>
-                                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-                                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+                                <button onClick={() => window.open(demo)} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
+                                <button onClick={() => window.open(code)} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
                             </div>
                         </div>
                     ))}
