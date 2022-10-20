61a27daf2bf061060d35bcefbb8c45dd22094817
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-10-20 09:23:04 +0530

    form submission fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,12 +2,6 @@ import React from 'react'
 
 export const Contact = () => {
 
-    const handleSubmit = (event) => {
-        event.target.name.value = ""
-        event.target.email.value = ""
-        event.target.message.value = ""
-    }
-
     return (
         <>
             <div name='contact' className='w-full h-screen bg-white p-4 text-gray-600 dark:bg-black transition-all'>
@@ -17,7 +11,7 @@ export const Contact = () => {
                         <p className='py-5 md:py-8 dark:text-gray-300'>To make a conversation with me. Submit the below details and I'll contact you.</p>
                     </div>
                     <div className='flex justify-center items-center'>
-                        <form onSubmit={handleSubmit} action="https://getform.io/f/4804e731-5f9e-4e3f-8b11-e2da49168e90" method='POST' className='flex flex-col w-full md:w-1/2'>
+                        <form action="https://getform.io/f/0a58c6b2-c093-4781-a8b6-9019e2300de2" method='POST' className='flex flex-col w-full md:w-1/2'>
                             <input res required type="text" name='name' placeholder='Enter your name' className='p-2 bg-transparent border-2 border-gray-400 rounded-md text-black focus:outline-none dark:text-white' />
                             <input required type="text" name='email' placeholder='Enter your email' className='my-4 p-2 bg-transparent border-2 border-gray-400 rounded-md text-black focus:outline-none dark:text-white' />
                             <textarea required name="message" rows="10" placeholder='Enter your message' className='p-2 bg-transparent border-2 border-gray-400 rounded-md text-black focus:outline-none dark:text-white'></textarea>
