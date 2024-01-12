5b0c712ce3e8b2c1def2b6f17a7b30fb666c38ad
SiddhantTotade <siddhanttotade.1994@gmail.com>
2024-01-12 11:13:43 +0530

    add : env

--- backend/sidfolio_project/settings.py
+++ backend/sidfolio_project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-u7vktk#a78k*e7g!mba!@o!(d%n4mxug1pxw#_ylh@#l@+66@x"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -60,7 +63,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "sidfolio_project.urls"
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173', 'http://localhost:4173']
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS").split(",")
 
 TEMPLATES = [
     {
