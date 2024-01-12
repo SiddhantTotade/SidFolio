99400ffbca13f8026b08fc83dfb176143ee11e13
SiddhantTotade <siddhanttotade.1994@gmail.com>
2024-01-12 14:36:24 +0530

    add : new build

--- backend/sidfolio_project/settings.py
+++ backend/sidfolio_project/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(", ")
 
 # Application definition
 
@@ -63,7 +62,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "sidfolio_project.urls"
 
-CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS").split(",")
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS").split(", ")
 
 TEMPLATES = [
     {
