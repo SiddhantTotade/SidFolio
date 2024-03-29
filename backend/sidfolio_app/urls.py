497a15b6d817c46254e5b5c3159ae350a87e61c8
SiddhantTotade <siddhanttotade.1994@gmail.com>
2024-03-29 13:23:01 +0530

    add : new branch

--- backend/sidfolio_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ProjectView, SkillsView, AboutView
-
-urlpatterns = [
-    path("project/", ProjectView.as_view(), name="project"),
-    path("about/", AboutView.as_view(), name="about"),
-    path("skill/", SkillsView.as_view(), name="skill")
-]
