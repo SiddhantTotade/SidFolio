f508a03af656a2f633d04946b4514ea713606a1c
SiddhantTotade <siddhanttotade.1994@gmail.com>
2024-03-31 00:11:32 +0530

    modify : UserData.sh

--- UserData.sh
+++ UserData.sh
@@ -8,7 +8,7 @@ GIT_REPO_URL="https://github.com/SiddhantTotade/SidFolio.git"
 #GIT_REPO_URL="https://<your_username>:<your_PAT>@github.com/codewithmuh/django-aws-ec2-autoscaling.git"
 
 # Replace {YOUR_PROJECT_MAIN_DIR_NAME} with your actual project directory name
-PROJECT_MAIN_DIR_NAME="SidFolio/backend/"
+PROJECT_MAIN_DIR_NAME="SidFolio/backend/backend/"
 
 # Clone repository
 git clone "$GIT_REPO_URL" "/home/ubuntu/$PROJECT_MAIN_DIR_NAME"
