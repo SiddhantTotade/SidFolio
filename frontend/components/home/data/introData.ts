2871f4901547a946e9dea56c718920be162451d6
SiddhantTotade <siddhanttotade.1994@gmail.com>
2024-04-01 11:01:09 +0530

    modify : text size

--- frontend/components/home/data/introData.ts
+++ frontend/components/home/data/introData.ts
@@ -12,11 +12,11 @@ export const textStyle = [
   {
     label: "Full Stack",
     gradient:
-      "text-transparent from-orange-500 via-purple-500 to-pink-500 text-5xl",
+      "text-transparent from-orange-500 via-purple-500 to-pink-500 text-5xl max-sm:text-4xl",
   },
   {
     label: "Developer",
     gradient:
-      "text-transparent from-indigo-500 via-pink-500 to-orange-500 text-7xl",
+      "text-transparent from-indigo-500 via-pink-500 to-orange-500 text-7xl max-sm:text-5xl",
   },
 ];
